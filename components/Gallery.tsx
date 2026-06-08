5a5fc78a4d2da8041458051182c4c89ea095e0c6
lvelasco-amapolatech <lvelasco@amapola.tech>
2026-06-08 16:56:32 -0400

    retoque de fotos listo

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,11 +1,11 @@
 export default function Gallery() {
   const images = [
     'photos/gallery/img-1.png',
-    'photos/gallery/img-2.jpeg',
+    'photos/gallery/img-2.png',
     'photos/gallery/img-3.jpeg',
     'photos/gallery/img-4.png',
     'photos/gallery/img-5.png',
-    'photos/gallery/img-6.jpeg',
+    'photos/gallery/img-6.png',
   ];
 
   return (
